228c8a2c39ae60ff9bf2928c8149011c7d6c3efa
volka <volka5091@gmail.com>
2025-06-07 15:31:00 +0900

    ReactorDebugAgent 추가 및 디버깅 프로필별 설정 및 Dockerfile 분리 및 설정 추가

--- src/main/java/com/volka/relayapi/RelayApiApplication.java
+++ src/main/java/com/volka/relayapi/RelayApiApplication.java
@@ -5,13 +5,41 @@ import com.volka.relayapi.properties.ExternalRelayApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @EnableConfigurationProperties({CustomNettyProperties.class, ExternalRelayApiProperties.class})
 @SpringBootApplication
 public class RelayApiApplication {
 
+    /**
+     * spring 컨텍스트 초기화 전에 적용해야함. -> 초기화 후면 이미 만들어진 쓰레드에 대한 블로킹 검출은 못함.
+     * @param args
+     */
     public static void main(String[] args) {
+        if (isDevProfiles()) {
+            System.out.println("[ReactorDebugAgent] INSTALL");
+            ReactorDebugAgent.init();
+            ReactorDebugAgent.processExistingClasses();
+        }
+
         SpringApplication.run(RelayApiApplication.class, args);
     }
 
+
+    private static boolean isDevProfiles() {
+        String profile = System.getProperty("spring.profiles.active");
+        return "local".equals(profile) || "dev".equals(profile);
+    }
+
+//    private static boolean isDevProfiles(String[] args) {
+//        String profile = null;
+//        for (String arg : args) {
+//            if (arg.contains("profiles.active=")) {
+//                profile = arg.split("=")[1];
+//                return profile.equals("local") || profile.equals("dev");
+//            }
+//        }
+//
+//        return true;
+//    }
 }
